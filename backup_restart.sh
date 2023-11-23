ce6f8db7cba48ec064f07e4baebe847b05824c84
Chikage Haruse <contact@chikage.net>
2023-11-23 22:28:57 +0900

    バックアップ先は引数で指定に変更

--- backup_restart.sh
+++ backup_restart.sh
@@ -2,14 +2,9 @@
 
 cd `dirname $0`
 
-# RCLONEの設定名
-RCLONE_NAME=r2
-# RCLONEのルートディレクトリ名（オブジェクトストレージならバケット名）
-RCLONE_ROOT_NAME=misskey-backup
-
-# 引数がmonthly, weekly, dailyのいずれかでなければ終了
-if [ "$1" != "monthly" ] && [ "$1" != "weekly" ] && [ "$1" != "daily" ]; then
-    echo "Argument must be monthly, weekly or daily."
+# 引数が空なら終了する
+if [ "$1" = "" ]; then
+    echo "引数が空です。"
     exit 1
 fi
 
@@ -18,5 +13,5 @@ docker compose pull &&\
 docker compose build --no-cache warp &&\
 docker compose down &&\
 echo Backup start... &&\
-XZ_OPT=-9 tar -Jcf - . | rclone rcat $RCLONE_NAME:$RCLONE_ROOT_NAME/$1/$(date "+%Y_%m_%d_%H_%M_%S").tar.xz &&\
+XZ_OPT=-9 tar -Jcf - . | rclone rcat $1/$(date "+%Y_%m_%d_%H_%M_%S").tar.xz &&\
 docker compose up -d
